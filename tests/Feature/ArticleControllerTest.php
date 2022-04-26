5c36909c46bdf540ad8c5051bb61a4908baf5528
RyosukeSomeya <rx.0.0618@gmail.com>
2022-04-27 00:14:46 +0900

    テスト追加

---
+++ tests/Feature/ArticleControllerTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ArticleControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testIndex()
+    {
+        $response = $this->get(route('articles.index'));
+
+        $response->assertStatus(200)->assertViewIs('articles.index');
+    }
+
+    // 未ログインのテスト
+    public function testGuestCreate()
+    {
+        $response = $this->get(route('articles.create'));
+        $response->assertRedirect(route('login'));
+    }
+
+    // ログイン時のテスト
+    public function testAuthCreate()
+    {
+        // ※ テストは Arrange[準備] → Action[実行] → Assert[検証]を意識する
+        // Userモデルのインスタンス生成しDBに保存 [準備]
+        $user = factory(User::class)->create();
+
+        // $userでログイン状態 [実行]
+        $response = $this->actingAs($user)->get(route('articles.create'));
+
+        // 記事投稿ページへアクセス [検証]
+        $response->assertStatus(200)->assertViewIs('articles.create');
+    }
+}
---
+++ tests/Feature/ArticleTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Article; // テストで使用するモデル
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ArticleTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testIsLikedByNull()
+    {
+        $article = factory(Article::class)->create();
+
+        $result = $article->isLikedBy(null);
+
+        $this->assertFalse($result);
+    }
+
+    // ユーザーがいいねしている
+    public function testIsLikedByTheUser()
+    {
+        $article = factory(Article::class)->create();
+        $user = factory(User::class)->create();
+        $article->likes()->attach($user);
+
+        $result = $article->isLikedBy($user);
+
+        $this->assertTrue($result);
+    }
+
+    // 別のユーザーが良いねしている
+    public function testIsLikedByAnother()
+    {
+        $article = factory(Article::class)->create();
+        $user = factory(User::class)->create();
+        $another = factory(User::class)->create();
+        $article->likes()->attach($another);
+
+        $result = $article->isLikedBy($user);
+
+        $this->assertFalse($result);
+    }
+}
